6c6430ff4551dd2ad285357fd3b8cce50a2861be
Tigran Mirvelyan <tabcd@mail.ru>
2022-08-11 17:16:26 +0400

    bitch

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, jsonify, request, render_template, redirect
 
 app = Flask(__name__)
 
@@ -7,16 +7,27 @@ def index():
     """
     TODO: Render the home page provided under templates/index.html in the repository
     """
-    return "TODO"
+    return render_template("index.html")
 
 @app.get("/search")
 def search():
-	"""
+    args = request.args.get("q")
+    return redirect(f"https://google.com/search?q= {args} ")
+    """
 	TODO:
 	1. Capture the word that is being searched
 	2. Seach for the word on Google and display results
 	"""
-    return "TODO"
+@app.get("/lucky")
+def lucky():
+    args = request.args.get("q")
+    return redirect(f"https://facebook.com/search?q= {args} ")
+    """
+	TODO:
+	1. Capture the word that is being searched
+	2. Seach for the word on Google and display results
+	"""
+    
 
 if __name__ == "__main__":
     app.run()
